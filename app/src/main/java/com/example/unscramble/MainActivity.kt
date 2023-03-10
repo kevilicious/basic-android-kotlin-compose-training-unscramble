bc74c71698e238bfe3e01692909d177753bd97ff
jtavva <jtavva@google.com>
2023-03-10 13:32:08 -0800

    Revert "M3 migration with custom theme"
    This reverts commit 356ec46b3ea37ac6b1f542dd215e31340ab8178a.

--- app/src/main/java/com/example/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.unscramble.ui.GameScreen
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.unscramble.ui.theme.UnscrambleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +22,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GameScreen()
                 }
             }
         }
